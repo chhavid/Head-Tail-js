8da047c9980874bfb2814a09a9c2fc7628880d2d
Chhavi <chhavid@users.noreply.github.com>
2022-05-20 12:24:48 +0530

    Head works for two lines.

--- test/testHead.js
+++ test/testHead.js
@@ -2,8 +2,13 @@ const assert = require('assert');
 const { head } = require('../src/head.js');
 
 describe('head', () => {
-  it('should give the first single line', () => {
+  it('should give the content of single line', () => {
     assert.strictEqual(head('hello'), 'hello');
     assert.strictEqual(head('bye'), 'bye');
+  });
+
+  it('should give the content with 2 lines', () => {
+    assert.strictEqual(head('hello\nworld'), 'hello\nworld');
+    assert.strictEqual(head('good\nbye'), 'good\nbye');
   });
 });
